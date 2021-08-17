be8482cc56bbed1f378e47ba6ed61435687730e1
Markus Korn <markus.korn@gmail.com>
2021-08-17 21:09:15 +0200

    workon: add dotfiles

--- .zsh_custom/workon.zsh
+++ .zsh_custom/workon.zsh
@@ -10,6 +10,12 @@ wo() {
             tmux-sessionizer ${HOME}/devel/bitbucket.org/burdastudios/
             code ${HOME}/.vs-code-workspaces/bunte.code-workspace
             ;;
+        dotfiles)
+            local gitdir=${HOME}/.dotfiles/
+            local workdir=${HOME}
+            tmux-sessionizer dotfiles
+            GIT_DIR=${gitdir} GIT_WORK_TREE=${workdir} code $(dotfiles ls-tree -r HEAD | grep blob | cut -f 2 | grep -v .vs-code-workspaces | awk -v home=${HOME} '{print home "/" $1}')
+            ;;
         *)
             echo "Error: unknown project '$1', exit"
             return 0
