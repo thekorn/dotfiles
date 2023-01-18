aa7c000d70b67c5c1e58dfbedd09381113e00fd2
Markus Korn <markus.korn@gmail.com>
2023-01-18 06:51:26 +0100

    zsh: new 'flutter-watch' function

--- .zsh_custom/functions.zsh
+++ .zsh_custom/functions.zsh
@@ -49,3 +49,16 @@ update() {
   echo "are there any changes?"
   dotfiles st
 }
+
+
+# flutter
+function flutter-watch(){
+  local PID_FILE="/tmp/tf$$.pid"
+  TMUX='' tmux new-session \;\
+    send-keys "flutter run --pid-file=$PID_FILE" Enter \;\
+    split-window -v \;\
+    send-keys "npx -y nodemon -e dart -x \"cat $PID_FILE | xargs kill -s USR1\"" Enter \;\
+    resize-pane -y 5 -t 1 \;\
+    select-pane -t 0 \;
+  rm $PID_FILE;
+}
